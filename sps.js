82339d60a5adfc704435c792755fad0174fa5be6
Kunal <100947485+heyyitskk@users.noreply.github.com>
2023-07-25 00:11:08 +0530

    add game function
    it lets the player play 5 rounds with the computer and after the match
    tells who won the match or if the match is a draw

--- sps.js
+++ sps.js
@@ -12,23 +12,52 @@ function getRPS(){
         return "scissors";
 }
 
-let computerSelection = getRPS();
-
-let playerSelection = prompt("Enter your choice").toLowerCase();
-
+let p, c;
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "scissors")
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        p++;
         return ("You Won! Rock beats scissors");
-    else if (playerSelection == "paper" && computerSelection == "rock")
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock"){
+        p++;
         return ("You Won! Paper beats rock");
-    else if (playerSelection == "scissors" && computerSelection == "paper")
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper"){
+        p++;
         return ("You Won! Scissors beats paper");
-    else if (playerSelection == "scissors" && computerSelection == "rock")
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock"){
+        c++;
         return ("You Lose! Rock beats scissors");
-    else if (playerSelection == "rock" && computerSelection == "paper")
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper"){
+        c++;
         return ("You Lose! Paper beats rock");
-    else if (playerSelection == "paper" && computerSelection == "scissors")
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors"){
+        c++;
         return ("You Lose! Scissors beats paper");
+    }
+    else 
+        return ("It's a tie!");
 }
 
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+    let computerSelection;
+    let playerSelection;
+    p = 0;
+    c = 0;
+    for(n = 0; n < 5; n++){
+        computerSelection = getRPS();
+        playerSelection = prompt("Enter your choice").toLowerCase();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    if(p > c)
+        console.log("Player won the match!");
+    else if(p < c)
+        console.log("Computer won the match!");
+    else
+        console.log("The match is a draw..");
+}
+
+game();
